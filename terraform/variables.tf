3295026c84c61f64bb2c6d8af786504e96e688c0
faizunnur <faizunnur71@gmail.com>
2026-03-09 01:11:20 +0600

    Update via DevOps Agent

---
+++ terraform/variables.tf
@@ -0,0 +1,117 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "royalben_02"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "production"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for nginx server"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "royalben_02-key"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "nginx_port" {
+  description = "Port on which nginx will listen"
+  type        = number
+  default     = 80
+}
+
+variable "nginx_ssl_port" {
+  description = "SSL port on which nginx will listen"
+  type        = number
+  default     = 443
+}
+
+variable "ssh_port" {
+  description = "SSH port for remote access"
+  type        = number
+  default     = 22
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the instance"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "allowed_http_cidr" {
+  description = "CIDR block allowed to access nginx over HTTP"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "allowed_https_cidr" {
+  description = "CIDR block allowed to access nginx over HTTPS"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root EBS volume in GB"
+  type        = number
+  default     = 20
+}
+
+variable "root_volume_type" {
+  description = "Type of the root EBS volume"
+  type        = string
+  default     = "gp2"
+}
+
+variable "associate_public_ip" {
+  description = "Whether to associate a public IP address with the instance"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default = {
+    Project     = "royalben_02"
+    ManagedBy   = "terraform"
+    Application = "nginx"
+  }
+}

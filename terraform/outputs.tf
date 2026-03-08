b6006dfd1620829d05238d66727c5e1936dd86b0
faizunnur <faizunnur71@gmail.com>
2026-03-09 01:38:53 +0600

    Auto-fix attempt 3: terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,39 +1,99 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance running nginx"
-  value       = aws_instance.nginx.id
+terraform/main.tf
+terraform {
+  backend "s3" {
+    bucket         = "devops-agent-tfstate-192626564093"
+    key            = "royalben_02/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.nginx.public_ip
+provider "aws" {
+  region = "us-east-1"
 }
 
-output "instance_public_dns" {
-  description = "The public DNS name of the EC2 instance"
-  value       = aws_instance.nginx.public_dns
+data "aws_vpc" "default" {
+  default = true
 }
 
-output "nginx_url" {
-  description = "The URL to access nginx"
-  value       = "http://${aws_instance.nginx.public_ip}"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-output "security_group_id" {
-  description = "The ID of the security group attached to the EC2 instance"
-  value       = aws_security_group.nginx_sg.id
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-output "instance_state" {
-  description = "The state of the EC2 instance"
-  value       = aws_instance.nginx.instance_state
+resource "aws_security_group" "nginx_sg" {
+  name        = "royalben_02-sg"
+  description = "DevOps Agent managed"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Project   = "royalben_02"
+    ManagedBy = "devops-agent"
+  }
 }
 
-output "availability_zone" {
-  description = "The availability zone of the EC2 instance"
-  value       = aws_instance.nginx.availability_zone
-}
+resource "aws_instance" "nginx" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = tolist(data.aws_subnets.default.ids)[0]
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
-output "project_name" {
-  description = "The name of the project"
-  value       = "royalben_02"
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name      = "royalben_02"
+    Project   = "royalben_02"
+    ManagedBy = "devops-agent"
+  }
 }
